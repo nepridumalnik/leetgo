2f249ec7dc24a2724f3a4a3198ab0e65eecc3d71
p.a.dudnik <p.a.dudnik@tbank.ru>
2026-08-11 11:10:38 +0700

    Upgrade generator

--- internal/task/generator.go
+++ internal/task/generator.go
@@ -105,7 +105,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testCase struct {
+	Skip bool
+}
+
 func Test_%s(t *testing.T) {
+	tests := []testCase{}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("example_%%d", i), func(t *testing.T) {
+			if test.Skip {
+				t.Skip("skipped")
+			}
+		})
+	}
 }
 `, pkgName, targetImport, data.CamelCaseRaw())
 
