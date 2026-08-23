3eb7688924ac57ae83f1316b056a927432640137
ne.pridumal.nik <ne.pridumal.nik@yandex.ru>
2026-08-23 18:25:42 +0700

    Task 2

--- medium/maximum_erasure_value/solution.go
+++ medium/maximum_erasure_value/solution.go
@@ -23,4 +23,26 @@ func maximumUniqueSubarrayV1(nums []int) int {
 	return longest
 }
 
-var MaximumUniqueSubarray func(nums []int) int = maximumUniqueSubarrayV1
+func maximumUniqueSubarrayV2(nums []int) int {
+	sum, left, longest := 0, 0, 0
+	indices := [10000]int{}
+
+	for i, n := range nums {
+		if idx := indices[n-1]; idx != 0 {
+			for left < idx {
+				indices[n-1] = 0
+				sum -= nums[left]
+				left++
+			}
+		}
+
+		indices[n-1] = i + 1
+		sum += nums[i]
+
+		longest = max(longest, sum)
+	}
+
+	return longest
+}
+
+var MaximumUniqueSubarray func(nums []int) int = maximumUniqueSubarrayV2
